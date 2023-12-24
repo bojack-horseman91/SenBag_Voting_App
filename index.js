40358872ce4658c1cdd7db3fdcf569a8061a09ef
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 11:02:41 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require("dotenv")
+const mangoos=require("mongoose")
+const location_router=require("./VotingLocation/VotingRouter")
+
+const app = express()
+const port = 3000
+dotenv.config()
+mangoos.connect(process.env.MONGODB_URL).then(()=>console.log("ok")).catch((err)=>console.log(err))
+var cors = require('cors');
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use("/api/location",location_router)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
